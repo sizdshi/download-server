89666a636c702496697e331a3ab9ac0766e91c5c
zyh <1143075643@qq.com>
2023-11-02 16:40:01 +0800

    first submission

---
+++ server/download-server/src/main/java/com/example/downloadserver/DownloadServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.downloadserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadServerApplication.class, args);
+	}
+
+}
---
+++ server/download-server/src/test/java/com/example/downloadserver/DownloadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.downloadserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
