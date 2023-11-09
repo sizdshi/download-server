b702bc8bba1d193e30bc1bf79f37c4a21d4b32ca
zyh <1143075643@qq.com>
2023-11-09 22:08:58 +0800

    update setting do,mapper,service&impl,controller

--- server/src/main/java/com/example/downloadserver/DownloadServerApplication.java
+++ server/src/main/java/com/example/downloadserver/DownloadServerApplication.java
@@ -1,9 +1,12 @@
 package com.example.downloadserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.example.downloadserver.mapper")
 public class DownloadServerApplication {
 
 	public static void main(String[] args) {
