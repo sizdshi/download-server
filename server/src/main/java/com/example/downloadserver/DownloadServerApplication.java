01919d5d13b3880ceac26bb74f2cdbcc8842aa02
Alexshi <827571424@qq.com>
2023-11-09 18:42:08 +0800

    [server] update main function

--- server/src/main/java/com/example/downloadserver/DownloadServerApplication.java
+++ server/src/main/java/com/example/downloadserver/DownloadServerApplication.java
@@ -1,9 +1,11 @@
 package com.example.downloadserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.downloadserver.mapper")
 public class DownloadServerApplication {
 
 	public static void main(String[] args) {
