9db1bd1cf2997caf702e072decd4da8ba0f94ace
zyh <1143075643@qq.com>
2023-11-13 23:17:07 +0800

    update FileController

--- server/src/main/java/com/example/downloadserver/controller/FileController.java
+++ server/src/main/java/com/example/downloadserver/controller/FileController.java
@@ -3,6 +3,7 @@ package com.example.downloadserver.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,24 +13,36 @@ import java.util.Comparator;
 @Controller
 @RequestMapping("/file")
 public class FileController {
-
     @GetMapping("/list")
-    public String fileList(@RequestParam(defaultValue = "/d/download") String directory,
-                           @RequestParam(defaultValue = "name") String sortBy, Model model) {
+    @PostMapping("/list")
+    public String fileList(@RequestParam(defaultValue = "/d/downloads") String directory,
+                           @RequestParam(defaultValue = "name") String sortBy,
+                           @RequestParam(defaultValue = "asc") String sortOrder,
+                           Model model) {
 
         File dir = new File(directory);
         File[] files = dir.listFiles();
 
-
         // 根据排序方式对文件进行排序
-        if (sortBy.equals("size")) {
-            Arrays.sort(files, Comparator.comparing(File::length));
-        } else if (sortBy.equals("name")) {
-            Arrays.sort(files, Comparator.comparing(File::getName));
-        } else if (sortBy.equals("ctime")) {
-            Arrays.sort(files, Comparator.comparingLong(File::lastModified));
+        boolean ascending = true;
+        if (sortOrder.equalsIgnoreCase("desc")) {
+            ascending = false;
         }
 
+        Comparator<File> comparator = null;
+        if (sortBy.equals("size")) {
+            comparator = Comparator.comparing(File::length);
+        } else if (sortBy.equals("name")) {
+            comparator = Comparator.comparing(File::getName);
+        } else if (sortBy.equals("ctime")) {
+            comparator = Comparator.comparingLong(File::lastModified);
+        }
+        if (comparator != null) {
+            if (!ascending) {
+                comparator = comparator.reversed();
+            }
+            Arrays.sort(files, comparator);
+        }
 
         model.addAttribute("files", files);
         return "fileList";
