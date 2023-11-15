fede2de31c37bd4f3768ff440bad8dd35de27e21
Alexshi <827571424@qq.com>
2023-11-15 21:20:40 +0800

    [server] change schema

---
+++ server/main/src/main/java/com/example/downloadserver/DownloaderMain.java
@@ -0,0 +1,18 @@
+package com.example.downloadserver;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Kenneth shi
+ * @Description:
+ **/
+@SpringBootApplication
+@MapperScan("com.example.downloadserver.mapper")
+public class DownloaderMain {
+    public static void main(String[] args) {
+        SpringApplication.run(DownloaderMain.class);
+    }
+}
---
+++ server/main/src/main/java/com/example/downloadserver/mapper/SettingMapper.java
@@ -0,0 +1,17 @@
+package com.example.downloadserver.mapper;
+
+
+import com.example.downloadserver.dataobject.SettingDO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface SettingMapper {
+
+    @Select("SELECT store_path as storePath,max_tasks as maxTasks,max_download_speed as maxDownloadSpeed,max_upload_speed as maxUploadSpeed FROM setting WHERE id=1")
+    SettingDO get();
+
+    @Update("update setting set store_path=#{storePath},max_tasks=#{maxTasks},max_download_speed=#{maxDownloadSpeed},max_upload_speed=#{maxUploadSpeed} where id=1")
+    int update(SettingDO settingDO);
+}
