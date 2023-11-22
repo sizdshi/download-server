c823635fbcfd113ef4f8e7ba15692f7c320847cf
Alexshi <827571424@qq.com>
2023-11-22 19:34:14 +0800

    [server] update directory

--- server/download/src/main/java/com/example/controller/FileController.java
+++ server/download/src/main/java/com/example/controller/FileController.java
@@ -33,22 +33,18 @@ public class FileController {
 
     @GetMapping("/down")
     public void fileDownload(){
-//        rangeDownload.download(fileUrl,savePath);
-//        System.out.println("下载成功");
 
-        //1、连接服务器，获取一个文件，获取文件的长度，在本地创建一个大小跟服务器文件一样大的临时文件
         MultipleThreadDownloadManager threadDownload = new MultipleThreadDownloadManager(fileUrl, "M:\\temp\\");
         //设置线程数
         threadDownload.setThreadCount(4);
-        //开启断点下载
-        //threadDownload.setBpDownload(true);
+
         //添加进度和网速监听
         threadDownload.addSpeedListener((s,progress)->{
             String m=String.format("%.2f",(double) s/1024/1024);
             String pro=String.format("%.2f",progress);
             System.out.println(m+"m/s--进度："+pro+"%");
         });
-        //由于ThreadDownload类也是个线程类，可以开启线程
+
         threadDownload.run();
 
     }
