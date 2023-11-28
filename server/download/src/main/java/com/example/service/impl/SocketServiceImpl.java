1950a3977e4ebe6594b544729a5cd1ca6821c2fe
zyh <zhouyuhuan@dreame.tech>
2023-11-28 08:22:00 +0800

    update SocketService & Impl

--- server/download/src/main/java/com/example/service/impl/SocketServiceImpl.java
+++ server/download/src/main/java/com/example/service/impl/SocketServiceImpl.java
@@ -9,21 +9,6 @@ import java.util.*;
 public class SocketServiceImpl implements SocketService {
 
     @Override
-    public void downloadFile(String url) throws IOException {
-        URL website = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) website.openConnection();
-
-        connection.setRequestMethod("GET");
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(connection.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }
-
-        in.close();
+    public void sendSocket(String url) throws IOException {
     }
 }
