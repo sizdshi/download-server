93032006574bc57263a1094c7c346b4b23582700
zyh <1143075643@qq.com>
2023-12-03 16:19:10 +0800

    update api.js & index.html & FileController

--- server/main/src/main/java/com/example/downloadserver/controller/FileController.java
+++ server/main/src/main/java/com/example/downloadserver/controller/FileController.java
@@ -19,14 +19,44 @@ public class FileController {
     @Autowired
     private SettingMapper settingMapper;
 
+    @GetMapping("/getStorePath")
+    @ResponseBody
+    public String getStorePath() {
+
+        String path = settingMapper.get().getStorePath();
+        path = new File(path).getAbsolutePath();
+        return path;
+    }
+
+    private static final Set<String> video = new HashSet<>();
+    private static final Set<String> image = new HashSet<>();
+
+    private static final Set<String> archive = new HashSet<>();
+
+    private static final Set<String> document = new HashSet<>();
+
+    static {
+        video.add("mp4");
+        video.add("mov");
+
+        image.add("png");
+        image.add("jpg");
+        image.add("gif");
+
+        archive.addAll(Arrays.asList("zip", "rar", "tar"));
+
+        document.addAll(Arrays.asList("pptx", "docx", "xlsx"));
+    }
+
     @GetMapping("/list")
     @ResponseBody
-    public List<FileInfo> fileList(@RequestParam(defaultValue = "/d/downloads") String directory,
+    public List<FileInfo> fileList(@RequestParam(defaultValue = "D:\\Downloads") String directory,
                                    @RequestParam(defaultValue = "createdAt") String sortBy,
                                    @RequestParam(defaultValue = "desc") String sortOrder,
+                                   @RequestParam(defaultValue = "all") String filter,
                                    HttpServletResponse response) throws IOException {
 
-        directory = settingMapper.get().getStorePath();
+
         // Check if directory is empty or null
         if (directory == null || directory.isEmpty()) {
             return null;
@@ -71,17 +101,46 @@ public class FileController {
         for (File f : files) {
             FileInfo fileInfo = new FileInfo();
 
+            String[] split = f.getName().split("\\.");
+            String ext = split[split.length - 1];
+            if(!judgeExt(ext, filter)){
+                continue;
+            }
+
             long createTime = f.lastModified();
             Date createDate = new Date(createTime);
 
-            fileInfo.setFilename(f.getName());
+            fileInfo.setName(f.getName());
             fileInfo.setSize(String.valueOf(f.length()));
-
             fileInfo.setCreatedAt(sdf.format(createDate));
+            fileInfo.setDirectory(f.isDirectory());
+            fileInfo.setPath("\\" + f.getName());
+
+
+
             filenames.add(fileInfo);
         }
 
 
         return filenames;
+    }
+
+
+    private static boolean judgeExt(String ext, String filter){
+        ext = ext.toLowerCase();
+        switch (filter){
+            case "all":
+                return true;
+            case "video":
+                return video.contains(ext);
+            case "image":
+                return image.contains(ext);
+            case "archive":
+                return archive.contains(ext);
+            case "document":
+                return document.contains(ext);
+            default:
+                return false;
+        }
     }
 }
