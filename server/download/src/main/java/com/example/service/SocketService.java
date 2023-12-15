c9bbc6b667db6d0ff52fa0579f5e2f19a7a1bf11
zyh <1143075643@qq.com>
2023-12-15 12:44:34 +0800

    update socketService & impl

--- server/download/src/main/java/com/example/service/SocketService.java
+++ server/download/src/main/java/com/example/service/SocketService.java
@@ -4,5 +4,20 @@ import java.io.IOException;
 
 public interface SocketService {
 
-    void sendSocket(String url) throws IOException;
+
+    /**
+     * 计算文件大小
+     * @return
+     */
+    long getFileSize() throws IOException;
+
+    /**
+     * 读取指定的chunk
+     * @param start
+     * @param end
+     * @return
+     */
+    byte[] readChunk(long start,long end) throws IOException;
+
+    void setUrl(String url);
 }
